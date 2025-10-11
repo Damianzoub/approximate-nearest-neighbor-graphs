ad16ec999ab061f6c54dedba793bcea0f3ab8e4b
Damian <d.zoumpos04@gmail.com>
2025-10-11 17:43:41 +0300

    Fixed hnsw demo

--- demos/hnsw_demo.py
+++ demos/hnsw_demo.py
@@ -2,22 +2,22 @@ import numpy as np
 import faiss 
 
 dimensions = 128
-nb = 1000
-nq = 3
+nb = 100000
+nq = 1000
 
 data = np.random.random((nb,dimensions)).astype("float32")
 queries = np.random.random((nq,dimensions)).astype("float32")
-print(f"Data: {data}")
-print(f"Queries: {queries}")
+
 
 #HNSW index 
 index = faiss.IndexHNSWFlat(dimensions,32)
-print(index)
 index.hnsw.efConstruction = 100
 index.hnsw.efSearch = 50
 
 index.add(data)
-
 k = 5
-distances , indices = index.search(queries,k)
-print(f"Nearest Neighbors: {indices}")
+distances , indices = index.search(queries,k) 
+for q in range(5):
+    print(f"Query: {q+1}")
+    print(f"Indices: {indices[q]}")
+    print(f"Distances: {distances[q]}")
