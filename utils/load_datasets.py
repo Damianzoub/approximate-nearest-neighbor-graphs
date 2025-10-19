0cfcf1aefbb8bb2d6d6dad79dd89bae2abe28973
Damian <d.zoumpos04@gmail.com>
2025-10-19 20:07:32 +0300

    created qps and recall on notebook

--- utils/load_datasets.py
+++ utils/load_datasets.py
@@ -1,7 +1,7 @@
 from utils.read_files import read_fvecs, read_ivecs
 
 def load_sift1M(base_dir):
-    xb = read_fvecs(f"{base_dir}/sift_base.fvecs")
-    xq = read_fvecs(f"{base_dir}/sift_query.fvecs")
-    xt = read_ivecs(f"{base_dir}/sift_groundtruth.ivecs")
+    xb = read_fvecs(f"{base_dir}/siftsmall_base.fvecs")
+    xq = read_fvecs(f"{base_dir}/siftsmall_query.fvecs")
+    xt = read_ivecs(f"{base_dir}/siftsmall_groundtruth.ivecs")
     return xb,xq,xt
