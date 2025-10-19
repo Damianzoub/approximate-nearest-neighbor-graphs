170e906547d0b7fd52149392cd3a18e2a5ec90ec
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-10-19 20:15:47 +0300

    Rename load_sift1M to load_siftsmall function

--- utils/load_datasets.py
+++ utils/load_datasets.py
@@ -1,6 +1,6 @@
 from utils.read_files import read_fvecs, read_ivecs
 
-def load_sift1M(base_dir):
+def load_siftsmall(base_dir):
     xb = read_fvecs(f"{base_dir}/siftsmall_base.fvecs")
     xq = read_fvecs(f"{base_dir}/siftsmall_query.fvecs")
     xt = read_ivecs(f"{base_dir}/siftsmall_groundtruth.ivecs")
