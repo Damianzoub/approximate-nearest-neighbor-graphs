dbf41e0c53e62e8d9be86212ff0120161f107fc4
Damianzoub <d.zoumpos04@gmail.com>
2025-11-16 19:08:38 +0200

    fixed the implementation of hnsw

--- hsnw_construction.py
+++ hsnw_construction.py
@@ -1,18 +1,19 @@
 import numpy as np 
-
+from heapq import heappush,heappop
 
 class HNSW:
 
-    def __init__(self,dim,M,efConstruction,metric='l2'):
+    def __init__(self,dim,M,efConstruction,metric='l2',seed=42):
         self.dim = dim 
-        self.M = M
+        self.M = int(M)
         self.M0 = 2* int(M)
         self.efConstruction = int(efConstruction)
         self.layers=[]
         self.vectors = {}
         self.metric = metric
         self.maxlevel = -1
-
+        self.rng = np.random.default_rng(seed) #more faster
+        self.entry_id = None 
 
 
     #return last level
@@ -32,8 +33,12 @@ class HNSW:
         pass 
 
     #probability of levels
-    def probab_levels(self,l=0.01):
-        return np.exp(-self.level/l)
+    # l = 1/ln(M)
+    def probab_levels(self,l): 
+        L=0 
+        while self.rng.random() < np.exp(-1.0/l):
+            L+=1
+        return L
     
     #calculate dist 
     def dist(self,a:np.ndarray,b:np.ndarray)->float:
@@ -43,7 +48,6 @@ class HNSW:
             return 1.0- float( np.dot(a,b)) 
         else:
             raise ValueError("Unknown metric")
-        pass
 
     #select neighbors
     def select_neighbors(self):
@@ -51,5 +55,18 @@ class HNSW:
 
     #returns the entry point ? maybe an id ?
     def entry_point(self):
+        return self.entry_id
+
+    #connect from node level and below
+    def _connect(self,u,v,level):
         pass
-    
+
+    def _prune_node(self,u,level):
+        pass
+    #beam search implementation
+    def beam_search(self):
+        pass
+
+
+
+#INFO: Assigning levels is purely probabilistic and independent of data distribution. and levels are assigned when nodes are added.
