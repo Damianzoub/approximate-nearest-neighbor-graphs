f1c5f018dbaeb9db6ae901b5400843bea05f3cad
Damianzoub <d.zoumpos04@gmail.com>
2026-03-12 11:11:29 +0200

    created the pip and adaef versions

---
+++ hnsw_pip_cpp/src/build/CMakeFiles/hnsw_pip.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hnsw_pip.dir/bindings.cpp.o"
+  "CMakeFiles/hnsw_pip.dir/bindings.cpp.o.d"
+  "CMakeFiles/hnsw_pip.dir/hnsw_pip.cpp.o"
+  "CMakeFiles/hnsw_pip.dir/hnsw_pip.cpp.o.d"
+  "hnsw_pip.cpython-311-darwin.so"
+  "hnsw_pip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hnsw_pip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
